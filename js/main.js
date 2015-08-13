4d7f2673e234deb7eb2da2f000b144a46ca6d719
joanne254 <joanne254@gmail.com>
2015-08-13 01:13:36 +0100

    Now this is looking much better. Still so far though.

--- js/main.js
+++ js/main.js
@@ -1,46 +1,78 @@
-function makeAPie () {
+function initCloud () {
 
-	var width = 960,
-	    height = 500,
-	    radius = Math.min(width, height) / 2;
+	d3.json("topics.json", function (error, json) {
+	  	
+	  	if (error) {
+	  		return console.warn(error);
+	  	}
 
-	var color = d3.scale.ordinal()
-	    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-	var arc = d3.svg.arc()
-	    .outerRadius(radius - 10)
-	    .innerRadius(0);
-
-	var pie = d3.layout.pie()
-	    .sort(null)
-	    .value(function(d) { return d.population; });
-
-	var svg = d3.select("body").append("svg")
-	    .attr("width", width)
-	    .attr("height", height)
-	  .append("g")
-	    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-	d3.csv("data.csv", function(error, data) {
-
-		data.forEach(function(d) {
-		  	d.population = +d.population;
-		});
-
-		var g = svg.selectAll(".arc")
-		    	.data(pie(data))
-		  	.enter().append("g")
-		    	.attr("class", "arc");
-
-		g.append("path")
-		    .attr("d", arc)
-		    .style("fill", function(d) { return color(d.data.age); });
-
-		g.append("text")
-			.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-			.attr("dy", ".35em")
-			.style("text-anchor", "middle")
-			.text(function(d) { return d.data.age; });
-
+	  	renderCloud(json.topics);
 	});
+}
+
+function renderCloud (topics) {
+	var dim = pickWidthHeight();
+	var fill = d3.scale.category20();
+
+	var layout = d3.layout.cloud()
+	    .size([dim.width, dim.height])
+	    .words(topics.map(function (d) {
+	    	return { text: d.label, size: 10 + Math.random() * 90, test: "haha"};
+	    }))
+	    .padding(5)
+	    .rotate(function () { return 0; })
+	    .font("Impact")
+	    .fontSize(function (d) { return d.size; })
+	    .on("end", drawCloudWords);
+
+	layout.start();
+
+	// Uses layout, fill, words
+	function drawCloudWords (words) {
+		d3.select("#cloud-container svg").remove()
+		d3.select("#cloud-container").append("svg")
+		    .attr("width", layout.size()[0])
+		    .attr("height", layout.size()[1])
+		.append("g")
+		    .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+		.selectAll("text")
+		    .data(words)
+		.enter().append("text")
+		    .style("font-size", function (d) { return d.size + "px"; })
+		    .style("font-family", "Impact")
+		    .style("fill", function (d, i) { return fill(i); })
+		    .attr("text-anchor", "middle")
+		    .attr("transform", function (d) {
+		      	return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+		    })
+		    .text(function (d) { return d.text; });
+	}
+}
+
+function pickWidthHeight() {
+	// Constants
+	var max_w = 1200,
+		min_w = 500,
+		max_h = 800
+		min_h = 500;
+
+	// Current window (inner) dimensions
+	var win_w = window.innerWidth,
+		win_h = window.innerHeight;
+
+	var w = win_w * 0.9,
+		h = win_w * 0.66;
+
+	if (w < min_w) {
+		w = min_w;
+	} else if (w > max_w) {
+		w = max_w;
+	}
+	if (h < min_h) {
+		h = min_h;
+	} else if (h > max_h) {
+		h = max_h;
+	}
+
+	return { width: w, height: h };
 }
